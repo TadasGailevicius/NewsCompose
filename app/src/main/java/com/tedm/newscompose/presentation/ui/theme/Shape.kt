c1a76ed054587698d6c6feca49a24b027d4cd0c1
Tadas <tadasgailevicius@yahoo.com>
2021-10-14 22:44:23 +0300

    Move ui package to presentation package

--- app/src/main/java/com/tedm/newscompose/presentation/ui/theme/Shape.kt
+++ app/src/main/java/com/tedm/newscompose/presentation/ui/theme/Shape.kt
@@ -1,4 +1,4 @@
-package com.tedm.newscompose.ui.theme
+package com.tedm.newscompose.presentation.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
