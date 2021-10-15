645eb2ae0cbd181f15a4538c9f06704353bccbf9
Tadas <tadasgailevicius@yahoo.com>
2021-10-15 16:29:29 +0300

    Update SplashScreen.kt Screen

--- app/src/main/java/com/tedm/newscompose/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/tedm/newscompose/presentation/splash/SplashScreen.kt
@@ -4,6 +4,7 @@ import android.view.animation.OvershootInterpolator
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
@@ -12,6 +13,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
 import com.tedm.newscompose.util.Constants
@@ -20,11 +23,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import com.tedm.newscompose.R
+import com.tedm.newscompose.presentation.util.Screen
 
 @Composable
 fun SplashScreen(
     navController: NavController,
     dispatcher: CoroutineDispatcher = Dispatchers.Main
+
 ) {
     val scale = remember {
         Animatable(0f)
@@ -35,9 +40,9 @@ fun SplashScreen(
     LaunchedEffect(key1 = true) {
         withContext(dispatcher) {
             scale.animateTo(
-                targetValue = 0.5f,
+                targetValue = 0.7f,
                 animationSpec = tween(
-                    durationMillis = 500,
+                    durationMillis = 750,
                     easing = {
                         overshootInterpolator.getInterpolation(it)
                     }
@@ -45,12 +50,13 @@ fun SplashScreen(
             )
             delay(Constants.SPLASH_SCREEN_DURATION)
             navController.popBackStack()
-            //navController.navigate(Screen.LoginScreen.route)
+            navController.navigate(Screen.MainScreen.route)
         }
     }
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center,
     ) {
         Image(
             painter = painterResource(id = R.drawable.logo),
