a16ac2b081af8f6baeda73e1a26b596b16fd01a5
Tadas <tadasgailevicius@yahoo.com>
2021-10-15 19:16:55 +0300

    Add StandardTextField.kt Component

---
+++ app/src/main/java/com/tedm/newscompose/presentation/components/StandardTextField.kt
@@ -0,0 +1,77 @@
+package com.tedm.newscompose.presentation.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTag
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import com.tedm.newscompose.presentation.util.TestTags.STANDARD_TEXT_FIELD
+
+@Composable
+fun StandardTextField(
+    modifier: Modifier = Modifier,
+    text: String = "",
+    hint: String = "",
+    maxLength: Int = 40,
+    error: String = "",
+    textStyle: TextStyle = TextStyle(
+        color = MaterialTheme.colors.onBackground
+    ),
+    singleLine: Boolean = true,
+    maxLines: Int = 1,
+    leadingIcon: ImageVector? = null,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    isPasswordToggleDisplayed: Boolean = keyboardType == KeyboardType.Password,
+    showPasswordToggle: Boolean = false,
+    onPasswordToggleClick: (Boolean) -> Unit = {},
+    onValueChange: (String) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        TextField(
+            value = text,
+            onValueChange = {
+                if (it.length <= maxLength) {
+                    onValueChange(it)
+                }
+            },
+            maxLines = maxLines,
+            textStyle = textStyle,
+            placeholder = {
+                Text(
+                    text = hint,
+                    style = MaterialTheme.typography.body1
+                )
+            },
+            isError = error != "",
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType
+            ),
+            visualTransformation = if (!showPasswordToggle && isPasswordToggleDisplayed) {
+                PasswordVisualTransformation()
+            } else {
+                VisualTransformation.None
+            },
+            singleLine = singleLine,
+            modifier = Modifier
+                .fillMaxWidth()
+                .semantics {
+                    testTag = STANDARD_TEXT_FIELD
+                }
+        )
+
+    }
+
+}
