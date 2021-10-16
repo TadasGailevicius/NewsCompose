9dc9e7e64118f752e8947287f7962bb4a99aa373
Tadas <tadasgailevicius@yahoo.com>
2021-10-16 21:33:11 +0300

    Add WeatherRepository.kt Repository

--- app/src/main/java/com/tedm/newscompose/util/Constants.kt
+++ app/src/main/java/com/tedm/newscompose/util/Constants.kt
@@ -3,4 +3,6 @@ package com.tedm.newscompose.util
 object Constants {
 
     const val SPLASH_SCREEN_DURATION = 0L
+    const val BASE_URL = "http://api.openweathermap.org/"
+    const val API_KEY = "7587eaff3affbf8e56a81da4d6c51d06"
 }
