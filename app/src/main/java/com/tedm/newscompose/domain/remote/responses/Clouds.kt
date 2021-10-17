4d3857a9495630092d5e92c2f56ae4b1326ed95a
Tadas <tadasgailevicius@yahoo.com>
2021-10-17 20:13:28 +0300

    Update WeatherResponse.kt Response

--- app/src/main/java/com/tedm/newscompose/domain/remote/responses/Clouds.kt
+++
@@ -1,5 +0,0 @@
-package com.tedm.newscompose.domain.remote.responses
-
-data class Clouds(
-    val all: Int
-)
--- app/src/main/java/com/tedm/newscompose/domain/remote/responses/Coord.kt
+++
@@ -1,6 +0,0 @@
-package com.tedm.newscompose.domain.remote.responses
-
-data class Coord(
-    val lat: Double,
-    val lon: Double
-)
