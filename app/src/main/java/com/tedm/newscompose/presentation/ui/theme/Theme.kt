2e21d3f9e4f40b03996981ca1e1b6c5d7cdba841
Tadas <tadasgailevicius@yahoo.com>
2021-10-19 17:32:04 +0300

    Update Theme Colors

--- app/src/main/java/com/tedm/newscompose/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/tedm/newscompose/presentation/ui/theme/Theme.kt
@@ -1,19 +1,11 @@
 package com.tedm.newscompose.presentation.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
+private val ColorPalette = darkColors(
     primary = LightGreen,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200
 
@@ -29,17 +21,10 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun NewsComposeTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
     MaterialTheme(
-        colors = colors,
+        colors = ColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
